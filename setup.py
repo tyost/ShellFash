74523294bce5ad6c82557d67f283ac0095446d70
Tim Yost <tyost@users.noreply.github.com>
2015-06-26 00:16:43 -0400

    Add basic implementation of setup.py to install this project and the wxPython 3 dependency. Key details like the descriptions and keywords are missing.

--- setup.py
+++ setup.py
@@ -4,5 +4,24 @@ License:
     This code is licensed to you under an open source license (MIT/X11).
     See the LICENSE file for details.
 '''
+from setuptools import setup, find_packages
 
-# TODO
+setup(
+    name='ShellFash',
+    version='0.0.1',
+    description='TODO',  # TODO: Finish
+    long_description='TODO',  # TODO: Finish
+    url='https://github.com/tyost/ShellFash',
+    author='Tim Yost',
+    license='MIT',
+    classifiers=[
+        'Topic :: Desktop Environment',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    keywords='', # TODO: Keywords
+    packages=find_packages(exclude=['test*']),
+    install_requires=['wxPython_Phoenix>=3'],
+)
